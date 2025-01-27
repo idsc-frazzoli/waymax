2d3f21d286518e8f9b66d5fedc1f7cf1d58981d6
jubilantrou <jubilantrou@163.com>
2025-01-27 12:29:55 +0100

    minor

--- waymax/metrics/penalize_headback_test.py
+++ waymax/metrics/penalize_headback_test.py
@@ -9,20 +9,28 @@ from waymax.utils import test_utils
 
 class PenalizeHeadbackMetricTest(tf.test.TestCase, parameterized.TestCase):
     def test_headback(self):
-        simulator_state = test_utils.simulator_state_with_offroad() # only velocity matters here
+        simulator_state = test_utils.simulator_state_with_offroad() # only velocity matters here, init yaw is 0
+        modified_sim_trajectory = dataclasses.replace(
+            simulator_state.sim_trajectory,
+            vel_x = simulator_state.sim_trajectory.vel_x.at[0,...].set(-1.0)
+        )
         modified_simulator_state = dataclasses.replace(
             simulator_state,
-            vel_x = simulator_state.vel_x.at[...,0].set(-1.0)
+            sim_trajectory = modified_sim_trajectory
         )
         result = penalize_headback.PenalizeHeadbackMetric().compute(modified_simulator_state)
         self.assertEqual(result.value.shape, (1,))
         self.assertEqual(result.value, 1.0)
 
     def test_not_headback(self):
-        simulator_state = test_utils.simulator_state_with_offroad() # only velocity matters here
+        simulator_state = test_utils.simulator_state_with_offroad() # only velocity matters here, init yaw is 0
+        modified_sim_trajectory = dataclasses.replace(
+            simulator_state.sim_trajectory,
+            vel_y = simulator_state.sim_trajectory.vel_y.at[0,...].set(-1.0)
+        )
         modified_simulator_state = dataclasses.replace(
             simulator_state,
-            vel_y = simulator_state.vel_y.at[...,0].set(-1.0)
+            sim_trajectory = modified_sim_trajectory
         )
         result = penalize_headback.PenalizeHeadbackMetric().compute(modified_simulator_state)
         self.assertEqual(result.value.shape, (1,))
