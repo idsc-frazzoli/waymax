d36e2c3c0c6be7d58695ed69e3dfae48399bdd6c
jubilantrou <jubilantrou@163.com>
2025-02-27 21:47:16 +0100

    minor

--- waymax/metrics/mitigate_oscillation_test.py
+++ waymax/metrics/mitigate_oscillation_test.py
@@ -9,15 +9,29 @@ from waymax.utils import test_utils
 
 
 class MitigateOscillationMetricTest(tf.test.TestCase, parameterized.TestCase):
+    def test_init_flag(self):
+        simulator_state = test_utils.simulator_state_with_offroad() # only action_history matters here
+        modified_simulator_state = dataclasses.replace(
+            simulator_state,
+            actions_history = datatypes.SDC_actions_history(
+                data=jnp.array([[1.0, 1.0],[-1.0, -1.0]]), valid=jnp.zeros((2,1), dtype=jnp.bool_)
+            )
+        )
+        result = mitigate_oscillation.MitigateOscillationMetric().compute(modified_simulator_state)
+        self.assertEqual(modified_simulator_state.timestep, 0)
+        self.assertEqual(result.value.shape, ())
+        self.assertEqual(result.value, 0.0)
+    
     def test_oscillation(self):
         simulator_state = test_utils.simulator_state_with_offroad() # only action_history matters here
         modified_simulator_state = dataclasses.replace(
             simulator_state,
-            action_history = datatypes.SDC_actions_history(
+            actions_history = datatypes.SDC_actions_history(
                 data=jnp.array([[1.0, 1.0],[-1.0, -1.0]]), valid=jnp.zeros((2,1), dtype=jnp.bool_)
-            )
+            ),
+            timestep = 1,
         )
-        result = mitigate_oscillation.PenalizeHeadbackMetric().compute(modified_simulator_state)
+        result = mitigate_oscillation.MitigateOscillationMetric().compute(modified_simulator_state)
         self.assertEqual(result.value.shape, ())
         self.assertEqual(result.value, -2.0)
 
@@ -25,11 +39,12 @@ class MitigateOscillationMetricTest(tf.test.TestCase, parameterized.TestCase):
         simulator_state = test_utils.simulator_state_with_offroad() # only action_history matters here
         modified_simulator_state = dataclasses.replace(
             simulator_state,
-            action_history = datatypes.SDC_actions_history(
+            actions_history = datatypes.SDC_actions_history(
                 data=jnp.array([[1.0, -2.0],[2.0, -1.0]]), valid=jnp.zeros((2,1), dtype=jnp.bool_)
-            )
+            ),
+            timestep = 1,
         )
-        result = mitigate_oscillation.PenalizeHeadbackMetric().compute(modified_simulator_state)
+        result = mitigate_oscillation.MitigateOscillationMetric().compute(modified_simulator_state)
         self.assertEqual(result.value.shape, ())
         self.assertEqual(result.value, 0.0)
 
