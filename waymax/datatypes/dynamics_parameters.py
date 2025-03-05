c3bb69e9c686edd2a6c6e584c295a7c0987259ce
Nicola Loi <nicloi@student.ethz.ch>
2025-03-05 16:04:32 +0100

    Add randomization of mass and cog height

--- waymax/datatypes/dynamics_parameters.py
+++ waymax/datatypes/dynamics_parameters.py
@@ -25,44 +25,93 @@ class DynamicsParams:
 
 @chex.dataclass
 class GokartDynamicsParams(DynamicsParams):
-        
-  Iz: jax.Array
-  front_paj_B: jax.Array
-  front_paj_C: jax.Array
-  front_paj_D: jax.Array
-  front_paj_E: jax.Array
-  rear_paj_B: jax.Array
-  rear_paj_C: jax.Array
-  rear_paj_D: jax.Array
-  rear_paj_E: jax.Array
-        
-  @classmethod
-  def zeros(cls, shape: Sequence[int]) -> "GokartDynamicsParams":
-        """Creates a GokartDynamicsParams containing zeros values."""
-        return cls(
-            Iz=jnp.zeros(shape, jnp.float32),
-            front_paj_B=jnp.zeros(shape, jnp.float32),
-            front_paj_C=jnp.zeros(shape, jnp.float32),
-            front_paj_D=jnp.zeros(shape, jnp.float32),
-            front_paj_E=jnp.zeros(shape, jnp.float32),
-            rear_paj_B=jnp.zeros(shape, jnp.float32),
-            rear_paj_C=jnp.zeros(shape, jnp.float32),
-            rear_paj_D=jnp.zeros(shape, jnp.float32),
-            rear_paj_E=jnp.zeros(shape, jnp.float32),
-        )
-        
-  @classmethod
-  def default_gokart(cls, shape: Sequence[int]) -> "GokartDynamicsParams":
-        """Creates a GokartDynamicsParams containing default values."""
-        return cls(
-            Iz=0.7*jnp.ones(shape, jnp.float32),
-            front_paj_B=17.17*jnp.ones(shape, jnp.float32),
-            front_paj_C=1.26*jnp.ones(shape, jnp.float32),
-            front_paj_D=0.8*jnp.ones(shape, jnp.float32),
-            front_paj_E=0.42*jnp.ones(shape, jnp.float32),
-            rear_paj_B=13.02*jnp.ones(shape, jnp.float32),
-            rear_paj_C=1.27*jnp.ones(shape, jnp.float32),
-            rear_paj_D=0.97*jnp.ones(shape, jnp.float32),
-            rear_paj_E=0.21*jnp.ones(shape, jnp.float32),
-        )
             
+    Iz: jax.Array
+    m: jax.Array
+    h: jax.Array
+    front_paj_B: jax.Array
+    front_paj_C: jax.Array
+    front_paj_D: jax.Array
+    front_paj_E: jax.Array
+    rear_paj_B: jax.Array
+    rear_paj_C: jax.Array
+    rear_paj_D: jax.Array
+    rear_paj_E: jax.Array
+        
+    @classmethod
+    def create_from_configs(
+        cls, shape: Sequence[int],
+        gk_geometry: 'GokartGeometry', model_params: 'GokartParams', paj_params: 'PajieckaParams'
+    ) -> "GokartDynamicsParams":
+        """Creates a GokartDynamicsParams containing the given values."""
+        Iz = model_params.Iz
+        m = gk_geometry.m
+        h = gk_geometry.h
+        front_paj_B = paj_params.front_paj.B
+        front_paj_C = paj_params.front_paj.C
+        front_paj_D = paj_params.front_paj.D
+        front_paj_E = paj_params.front_paj.E
+        rear_paj_B = paj_params.rear_paj.B
+        rear_paj_C = paj_params.rear_paj.C
+        rear_paj_D = paj_params.rear_paj.D
+        rear_paj_E = paj_params.rear_paj.E
+        return cls(
+            Iz=Iz*jnp.ones(shape, jnp.float32),
+            m=m*jnp.ones(shape, jnp.float32),
+            h=h*jnp.ones(shape, jnp.float32),
+            front_paj_B=front_paj_B*jnp.ones(shape, jnp.float32),
+            front_paj_C=front_paj_C*jnp.ones(shape, jnp.float32),
+            front_paj_D=front_paj_D*jnp.ones(shape, jnp.float32),
+            front_paj_E=front_paj_E*jnp.ones(shape, jnp.float32),
+            rear_paj_B=rear_paj_B*jnp.ones(shape, jnp.float32),
+            rear_paj_C=rear_paj_C*jnp.ones(shape, jnp.float32),
+            rear_paj_D=rear_paj_D*jnp.ones(shape, jnp.float32),
+            rear_paj_E=rear_paj_E*jnp.ones(shape, jnp.float32),
+        )
+        
+    def validate(self):
+        """Validates shape and type."""
+        chex.assert_equal_shape(
+            [
+                self.Iz,
+                self.m,
+                self.h,
+                self.front_paj_B,
+                self.front_paj_C,
+                self.front_paj_D,
+                self.front_paj_E,
+                self.rear_paj_B,
+                self.rear_paj_C,
+                self.rear_paj_D,
+                self.rear_paj_E,
+            ]
+        )
+        chex.assert_type(
+            [
+                self.Iz,
+                self.m,
+                self.h,
+                self.front_paj_B,
+                self.front_paj_C,
+                self.front_paj_D,
+                self.front_paj_E,
+                self.rear_paj_B,
+                self.rear_paj_C,
+                self.rear_paj_D,
+                self.rear_paj_E,
+            ],
+            [
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+                jnp.float32,
+            ],
+        )
+                
